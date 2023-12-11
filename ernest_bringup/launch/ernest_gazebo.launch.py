cb161d6fa2bb6e4995965b6b0e3f67cef92473e9
mariobo8 <m.bozza@outlook.com>
2023-12-11 12:06:51 -0800

    added the model to gazebo

--- ernest_bringup/launch/ernest_gazebo.launch.py
+++ ernest_bringup/launch/ernest_gazebo.launch.py
@@ -23,11 +23,34 @@ from launch.event_handlers import OnProcessExit
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
+from ament_index_python.packages import get_package_prefix
 
 from launch_ros.actions import Node
 
+import xacro
 
 def generate_launch_description():
+    pkg_box_bot_gazebo = get_package_share_directory('ernest_bringup')
+    description_package_name = "ernest_description"
+    install_dir = get_package_prefix(description_package_name)
+
+# This is to find the models inside the models folder in my_box_bot_gazebo package
+    gazebo_models_path = os.path.join(pkg_box_bot_gazebo, 'models')
+    if 'GAZEBO_MODEL_PATH' in os.environ:
+        os.environ['GAZEBO_MODEL_PATH'] = os.environ['GAZEBO_MODEL_PATH'] + \
+            ':' + install_dir + '/share' + ':' + gazebo_models_path
+    else:
+        os.environ['GAZEBO_MODEL_PATH'] = install_dir + \
+            "/share" + ':' + gazebo_models_path
+
+    if 'GAZEBO_PLUGIN_PATH' in os.environ:
+        os.environ['GAZEBO_PLUGIN_PATH'] = os.environ['GAZEBO_PLUGIN_PATH'] + \
+            ':' + install_dir + '/lib'
+    else:
+        os.environ['GAZEBO_PLUGIN_PATH'] = install_dir + '/lib'
+
+    print("GAZEBO MODELS PATH=="+str(os.environ["GAZEBO_MODEL_PATH"]))
+    print("GAZEBO PLUGINS PATH=="+str(os.environ["GAZEBO_PLUGIN_PATH"]))
         
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
@@ -48,7 +71,7 @@ def generate_launch_description():
             " ",
             "use_gazebo:=true",
             " ",
-            "use_lowpoly:=true",
+            "use_lowpoly:=false",
         ]
     )
     robot_description = {"robot_description": robot_description_content}
@@ -65,32 +88,8 @@ def generate_launch_description():
                         arguments=['-topic', 'robot_description',
                                    '-entity', 'ernest'],
                         output='screen')
-
-    load_joint_state_controller = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'start',
-             'joint_state_broadcaster'],
-        output='screen'
-    )
-
-    load_diff_drive_controller = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'start',
-             'diff_drive'],
-        output='screen'
-    )
     
     return LaunchDescription([
-        RegisterEventHandler(
-            event_handler=OnProcessExit(
-                target_action=spawn_entity,
-                on_exit=[load_joint_state_controller],
-        )
-    ),
-        RegisterEventHandler(   
-            event_handler=OnProcessExit(
-                target_action=spawn_entity,
-                on_exit=[load_diff_drive_controller],
-        )
-    ),
         gazebo,
         node_robot_state_publisher,
         spawn_entity,
