202847ea348df3e23d10812c45fc26172394c5a8
mariobo8 <m.bozza@outlook.com>
2023-12-19 15:24:06 -0800

    Added simple controller for the joints

--- ernest_controller/src/joint_traj_controller.cpp
+++ ernest_controller/src/joint_traj_controller.cpp
@@ -14,22 +14,43 @@ public:
             std::chrono::milliseconds(1),  // adjust the frequency as needed
             [this]() {
                 auto joint_trajectory_msg = buildJointTrajectoryMessage();
+                RCLCPP_INFO(
+                        get_logger(),
+                        "Trajectory message:\n"
+                        "Header stamp: %ld.%09d\n"
+                        "Joint names: %s\n"
+                        "Positions: [%f, %f, %f, %f, %f]\n"
+                        "Time from start: %ld.%09d",
+                        joint_trajectory_msg.header.stamp.sec,
+                        joint_trajectory_msg.header.stamp.nanosec,
+                        joint_trajectory_msg.joint_names[0].c_str(),
+                        joint_trajectory_msg.points[0].positions[0],
+                        joint_trajectory_msg.points[0].positions[1],
+                        joint_trajectory_msg.points[0].positions[2],
+                        joint_trajectory_msg.points[0].positions[3],
+                        joint_trajectory_msg.points[0].positions[4],
+                        joint_trajectory_msg.points[0].time_from_start.sec,
+                        joint_trajectory_msg.points[0].time_from_start.nanosec
+                        );
                 joint_trajectory_publisher_->publish(joint_trajectory_msg);
             });
+        
     }
 
-private:
+
     trajectory_msgs::msg::JointTrajectory buildJointTrajectoryMessage() {
         trajectory_msgs::msg::JointTrajectory joint_trajectory_msg;
-        joint_trajectory_msg.header.stamp = now();
+        joint_trajectory_msg.header.stamp = rclcpp::Time(0, 0);
 
         // Fill in joint names
         joint_trajectory_msg.joint_names = {"front_left_steer_joint", "front_right_steer_joint", "rear_left_steer_joint", "rear_right_steer_joint", "pivot_joint"};
 
         // Fill in joint positions (angles)
         joint_trajectory_msg.points.resize(1);
-        joint_trajectory_msg.points[0].positions = {1.0, 1.0, 0.0, 0.0, 0.0};  // adjust joint angles
-
+        joint_trajectory_msg.points[0].positions = {0.0, 0.0, 0.0, 0.0, 0.0};  // adjust joint angle
+    
+        joint_trajectory_msg.points[0].time_from_start.sec = 0;
+        joint_trajectory_msg.points[0].time_from_start.nanosec = 0;
         return joint_trajectory_msg;
     }
 
